b26fe1b48968681b68d00c4c3bf4e993aaa8d438
Ladybug <joanarochadasilva96@gmail.com>
2022-04-18 09:44:06 -0300

    Commit da nova pasta relativa a criação do meu primeiro site php

---
+++ Spread Full Stack Developer/Primeiros passos para o desenvolvimento web/página.php
@@ -0,0 +1,16 @@
+<html>
+
+	<head>
+		<title> Criando pela primeira vez em PHP! </title>
+	</head>
+	
+	<body>
+	
+		<?php
+			for ($i = 0 ; $i <10; $i++) {
+				print ("Linha número " . $i . "<br />"); 
+			}
+		?>
+		
+	</body>
+</html>
